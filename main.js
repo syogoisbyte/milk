51e9ceead0d222c59b9f8dbdde43974d817a9d0a
syogoisbyte <syogoisbyte@users.noreply.github.com>
2014-12-05 16:15:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,34 +2,34 @@ var milkcocoa = new MilkCocoa("https://io-gi3b4t3v4.mlkcca.com");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
 var chatDataStore = milkcocoa.dataStore("chat");
 
-//フォームの情報を記録しておく変数.
-var textArea, board,textName;
+// フォームの情報を記録しておく変数
+var textArea, board,textName;                   // ←これ
 
-//ページが読み込み終わったときの処理.
+// ページが読み込み終わったときの処理
 window.onload = function(){
   textArea = document.getElementById("msg");
   board = document.getElementById("board");
-  textName = document.getElementById("textName");
+  textName = document.getElementById("txtName");  // ←これ
 }
 
 function clickEvent(){
   var text = textArea.value;
-  sendText(text,textName.value);
+  sendText(text,textName.value);                  // ←これ
 }
 
-function sendText(text,nm){
-  chatDataStore.push({uname : nm,content : text},function(data){
+function sendText(text,nm){                       // ←これ
+  chatDataStore.push({uname : nm,content : text},function(data){  // ←これ
     console.log("送信完了!");
     textArea.value = "";
   });
 }
 
 chatDataStore.on("push",function(data){
-  addText(data.value);
+  addText(data.value);     // ←これ
 });
 
 function addText(text){
   var msgDom = document.createElement("li");
-  msgDom.innerHTML = text.uname+":"+text.content;
+  msgDom.innerHTML = text.uname+":"+text.content; // ←これ
   board.insertBefore(msgDom, board.firstChild);
 }
